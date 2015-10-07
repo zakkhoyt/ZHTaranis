59d688e66f047d5304edaf1046fb965e79ba4380
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-10-06 20:40:55 -0700

    Initial Commit

---
+++ ZHTaranis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHTaranis
+//
+//  Created by Zakk Hoyt on 10/6/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
